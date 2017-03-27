a7bb98da40821fed59f2d92e9fa9723779d12713
Juan Fernando Moreno <Juan Fernando Moreno>
2017-03-27 16:57:08 -0500

    primer commit para heroku

--- login/settings.py
+++ login/settings.py
@@ -124,8 +124,3 @@ DATABASES['default'] = dj_database_url.config()
 ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
